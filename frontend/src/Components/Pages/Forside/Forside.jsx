c883e8f253063fe07dc63aa6e5d1508836af1d95
Oliver-Bredmose <olbr1@elev.techcollege.dk>
2026-06-01 13:25:46 +0200

    har brug for hjælp

--- frontend/src/Components/Pages/Forside/Forside.jsx
+++ frontend/src/Components/Pages/Forside/Forside.jsx
@@ -1,26 +1,38 @@
+import { useEffect, useState } from "react";
 import style from "./Forside.module.scss";
 import SearchField from "../../SearchBar/SearchField";
 import { Cards } from "../../Cards/Cards";
 import { Filter } from "../../Filter/Filter";
 
-
 export default function Forside() {
-return (
+  const [boder, setBoder] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/booths")
+      .then((res) => res.json())
+      .then((data) => setBoder(data));
+  }, []);
+
+  return (
     <div className={style.cardscontent}>
-    <div className={style.Headertxt}>
+      <div className={style.Headertxt}>
         <h3>Vejboden</h3>
         <h2>Find Din nærmeste Vejbod</h2>
         <h4>Lokale sælgere nær dig</h4>
-        <SearchField/>
-    <br />
+        <SearchField />
+        <br />
+      </div>
+      <br />
+      <Filter />
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "1rem", padding: "1rem" }}>
+        {boder.map((bod) => (
+          <Cards
+            key={bod.id}
+            title={bod.name}
+            distance={bod.latitude + ", " + bod.longitude}
+          />
+        ))}
+      </div>
     </div>
-    <br />
-        <Filter/>
-        <Cards/>
-    </div>
-
-    
-    
-
-);
+  );
 }
