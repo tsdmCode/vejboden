b631a299ffd0547becf02466dfd3ebb5784a0faa
Oliver-Bredmose <olbr1@elev.techcollege.dk>
2026-06-01 08:27:37 +0200

    login

--- frontend/src/Components/Pages/Login/Login.jsx
+++ frontend/src/Components/Pages/Login/Login.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import style from "./Login.module.scss";
 
-
 export default function LogInd() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
 
-  return (
-    <div className={style.page}>
+const handleContinue = () => {
+  if (!/[^@]+@[^@]+/.test(email)) {
+    setEmailError("Email skal indeholde @");
+    return;
+  }
+  setEmailError("");
+  navigate("/forside");
+};
+
+return (
+  <div className={style.page}>
       <div className={style.top}>
         <h1>Log ind</h1>
         <p>Velkommen tilbage til vejboden.</p>
@@ -14,20 +25,21 @@ export default function LogInd() {
 
       <div className={style.form}>
         <label>Email</label>
-              <input
-                className={style.input}
-                type="email"
-                placeholder="mads@eksempel.dk"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  setEmailError("");
-                }}
-              />
-              {emailError && <p className={style.error}>{emailError}</p>}
-              <button className={style.btnPrimary} onClick={handleContinue}>Fortsæt</button>
-              <button className={style.btnSecondary} onClick={() => setStep(0)}>Tilbage</button>
-            </div>
+        <input
+          className={style.input}
+          type="email"
+          placeholder="mads@eksempel.dk"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setEmailError("");
+          }}
+          />
+        {emailError && <p className={style.error}>{emailError}</p>}
+        <button className={style.btnPrimary} onClick={handleContinue}>Fortsæt</button>
+        <button className={style.btnSecondary} onClick={() => navigate("/")}>Tilbage</button>
+      </div>
     </div>
   );
-}
+};
+  
