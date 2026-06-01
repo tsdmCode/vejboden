d2046f9299a6ebc09943c7aa9a3ee8895768ae5f
Oliver-Bredmose <olbr1@elev.techcollege.dk>
2026-06-01 12:07:53 +0200

    start on forside

--- frontend/src/Components/Pages/Forside/Forside.jsx
+++ frontend/src/Components/Pages/Forside/Forside.jsx
@@ -1,5 +1,22 @@
+import style from "./Forside.module.scss";
+import SearchField from "../../SearchBar/SearchField";
+import { Cards } from "../../Cards/Cards";
+
+
 export default function Forside() {
 return (
-    <div>Forside</div>
+    <div className={style.cardscontent}>
+    <div className={style.Headertxt}>
+        <h3>Vejboden</h3>
+        <h2>Find Din nærmeste Vejbod</h2>
+        <h4>Lokale sælgere nær dig</h4>
+        <SearchField/>
+    </div>
+        <Cards/>
+    </div>
+
+    
+    
+
 );
 }
