4882a703c743780dc61caabc305e30b924240583
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2026-06-01 12:19:34 +0200

    Fixed google oauth og opdateret vores modeller til at have lukke åbningstider

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import { userRoutes } from './routes/userRoutes.js';
 import { boothRoutes } from './routes/boothRoutes.js';
 import { ratingsRoutes } from './routes/ratingsRoutes.js';
 import { authRoutes } from './routes/authRoutes.js';
-
+import session from "express-session"
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -16,12 +16,22 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-passport.initialize();
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'your_session_secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/booths', boothRoutes);
 app.use('/api/ratings', ratingsRoutes);
-app.use("api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
